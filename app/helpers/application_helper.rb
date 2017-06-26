032ecd8854a1f0b59fa816c7433ddaa2fd4d3ba4
Armando Cerrillo <armando@cerrillomedia.org>
2017-06-26 08:11:45 +0000

    add expriation method to application_helper & update mobile view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+    
+    def expiration(item)
+        date = item.created_at
+        expiration = "expires in"
+        css_level = 'text-muted'
+        ago = ''
+        
+        case 
+            when date <= 7.days.ago
+                expiration = "expired"
+                css_level = 'text-danger'
+                ago = " ago"
+            when date <= 6.days.ago
+                css_level = 'text-danger'
+            when date <= 4.days.ago
+                css_level = 'text-warning'
+            when date < 3.day.ago
+                css_level = 'text-info'
+            end
+            
+        "<span class=\"col-xs-6 #{css_level} text-xs-left text-right\">
+            #{expiration}: <strong >#{distance_of_time_in_words(date + 7.day, Time.zone.now)}#{ago}</strong>
+        </span>".html_safe
+    end
+    
+    def format_datetime(type)
+        (type.completed_at || type.updated_at).strftime("%m/%d/%Y at%l:%M:%p " )
+    end
+    
 end
